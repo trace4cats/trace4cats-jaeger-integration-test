9714e7da3360bb131fb29100d6978e2a716c299f
Scala Steward <me@scala-steward.org>
2021-07-19 10:10:06 +0200

    Update logback-classic to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
     val circe = "0.14.1"
     val http4s = "0.23.0-RC1"
-    val logback = "1.2.3"
+    val logback = "1.2.4"
 
     val kindProjector = "0.13.0"
     val betterMonadicFor = "0.3.1"
