c6ae8ebb3e32019005fc765cd55aa77e9925474f
Scala Steward <me@scala-steward.org>
2021-09-04 02:47:26 +0200

    Update kind-projector to 0.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val http4s = "0.23.3"
     val logback = "1.2.5"
 
-    val kindProjector = "0.13.1"
+    val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
   }
 
